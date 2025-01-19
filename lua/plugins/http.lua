a9b4f5edc0c0533d5daecd005c16bb782a1b6619
Thales <thalesbrno@gmail.com>
2025-01-19 10:49:28 -0300

    feat: add rest http client

---
+++ lua/plugins/http.lua
@@ -0,0 +1,22 @@
+return {
+  "rest-nvim/rest.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      table.insert(opts.ensure_installed, "http")
+      vim.keymap.set(
+        "n",
+        "<leader>rr",
+        "<cmd>Rest run<cr>",
+        { noremap = true, silent = true, desc = "Rest run request" }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>rl",
+        "<cmd>Rest logs<cr>",
+        { noremap = true, silent = true, desc = "Rest request logs" }
+      )
+    end,
+  },
+}
