012a0abeb99661fa2788e5c513c1aad0805aaa6c
Thales <thalesbrno@gmail.com>
2025-02-27 09:34:03 -0300

    feat: add nui quit menu

--- lua/plugins/nui.lua
+++ lua/plugins/nui.lua
@@ -3,6 +3,9 @@ return {
     "MunifTanjim/nui.nvim",
     config = function()
       local Input = require("nui.input")
+      local Menu = require("nui.menu")
+      local event = require("nui.utils.autocmd").event
+
       local create_input_style = function(title)
         return {
           position = "30%",
@@ -45,6 +48,52 @@ return {
 
         new_file_input:mount()
       end, { desc = "Nui Open new file with prompt" })
+
+      -- Quit prompt
+      vim.keymap.set("n", "<leader>Q", function()
+        local menu = Menu({
+          position = "50%",
+          size = {
+            width = 30,
+            height = 3,
+          },
+          border = {
+            style = "rounded",
+            text = {
+              top = "Quit Options",
+              top_align = "center",
+            },
+          },
+          win_options = {
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+          },
+        }, {
+          lines = {
+            Menu.item("Cancel"),
+            Menu.item("Save and Quit"),
+            Menu.item("Discard and Quit"),
+          },
+          max_width = 30,
+          keymap = {
+            focus_next = { "j", "<Down>" },
+            focus_prev = { "k", "<Up>" },
+            close = { "<Esc>" },
+            submit = { "<CR>" },
+          },
+          on_submit = function(item)
+            if item.text == "Save and Quit" then
+              vim.cmd("wqa") -- Save all and quit
+            elseif item.text == "Discard and Quit" then
+              vim.cmd("qa!") -- Force quit, discard changes
+            end
+          end,
+        })
+
+        menu:mount()
+        menu:on(event.BufLeave, function()
+          menu:unmount()
+        end)
+      end, { desc = "Nui Quit Options Menu" })
     end,
   },
 }
